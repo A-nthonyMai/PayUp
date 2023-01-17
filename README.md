5961a419e91741823661937905140d25935bb612
Anthony Mai <Anthony.Mai@hotmail.com>
2023-01-17 15:17:25 +1100

    Bootstrapping application using NextJS

---
+++ README.md
@@ -0,0 +1,2 @@
+## Overview
+This application allows users to split a bill between a number of people. Users will be able to decide per item who it should be split with.
---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Pay Up!</title>
+        <meta name="description" content="Split a bill with friends" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <div>
+          <p>
+            Hello World!
+          </p>
+        </div>
+      </main>
+    </>
+  )
+}
