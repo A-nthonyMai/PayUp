f184376b538c265a0f61198a908c1117434cff1c
Anthony Mai <Anthony.Mai@hotmail.com>
2023-01-18 23:15:45 +1100

    Created header and layout components, added TailwindCSS

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className='bg-deep-purple text-bon-jour-purple h-screen w-full '>
         <Main />
         <NextScript />
       </body>
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Header from 'components/header'
+import Layout from 'components/layout'
+
+export default function Home() {
+  return (
+    <Layout>
+      <main className='flex flex-col'>
+        <Header />
+      </main>
+    </Layout>
+  )
+}
