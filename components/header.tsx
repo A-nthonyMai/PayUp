a6925290125e13d8af7ccc23449b80271a61d631
Anthony Mai <Anthony.Mai@hotmail.com>
2023-01-19 09:33:12 +1100

    Fixed margin on mobile, added shadow

--- components/header.tsx
+++ components/header.tsx
@@ -3,8 +3,8 @@ import Link from "next/link"
 
 const Header = () => {
     return (
-        <div className="bg-don-juan-purple h-16">
-            <div className="w-48 h-16 ml-[25%]">
+        <div className="bg-don-juan-purple h-16 shadow-2xl">
+            <div className="w-48 h-16 mx-2 sm:ml-[25%]">
                 <Link href="https://anto.au">
                     <div className="w-16 h-16 relative">
                         <Image className="rounded-full" src="/images/anthony-mai-icon.png" alt="Photo of Anthony Mai" fill />
