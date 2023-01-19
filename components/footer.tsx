648c01a01808d19c5e2470173275599c3a0683a1
Anthony Mai <Anthony.Mai@hotmail.com>
2023-01-19 21:36:39 +1100

    Updated social links and colours

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,24 +3,22 @@ import SocialButton from "./socialButton"
 
 const Footer = () => {
     return (
-        <div className="bg-thunder-purple mt-auto h-12 flex items-center justify-center text-xl">
-            <p>Created by <Link className="hover:underline" href="https://anto.au">Anthony Mai</Link></p>
-            <div className="flex flex-row">
+        <div className="bg-dark-purple mt-auto h-12 flex items-center justify-center text-xl">
+            <p className="mr-2">Created by <Link className="hover:underline" href="https://anto.au">Anthony Mai</Link></p>
+            <div className="flex flex-row border-l-2 border-thistle-purple">
+                <SocialButton
+                    pathToIcon="/icons/web-icon.svg"
+                    link="https://anto.au"
+                    additionalCSS="ml-2"
+                />
                 <SocialButton
                     pathToIcon="/icons/linkedIn-icon.svg"
                     link="https://www.linkedin.com/in/a-nthonymai"
-                    invert={true}
                     additionalCSS="ml-2"
                 />
                 <SocialButton
                     pathToIcon="/icons/github-icon.svg"
                     link="https://www.github.com/a-nthonymai/pay-up"
-                    additionalCSS="ml-2"
-                />
-                <SocialButton
-                    pathToIcon="/icons/email-icon.svg"
-                    link="mailto:hello@anto.au"
-                    invert={true}
                     additionalCSS="ml-2"
                 />
             </div>
